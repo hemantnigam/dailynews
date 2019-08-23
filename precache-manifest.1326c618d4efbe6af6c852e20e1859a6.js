81a09765cdea0dd3bac5b53d68b00cb926369715
Hemant Nigam <hemant.nigam@nagarro.com>
2019-08-23 15:55:39 +0530

    Updates

---
+++ precache-manifest.1326c618d4efbe6af6c852e20e1859a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3abb14c53d4f18197c8e8330d01149",
+    "url": "/dailynews/index.html"
+  },
+  {
+    "revision": "0ad3fcb59b7e3395c31a",
+    "url": "/dailynews/static/css/main.9bbe47e2.chunk.css"
+  },
+  {
+    "revision": "3cb9810fc648a990545f",
+    "url": "/dailynews/static/js/2.a5a08222.chunk.js"
+  },
+  {
+    "revision": "0ad3fcb59b7e3395c31a",
+    "url": "/dailynews/static/js/main.dcba5051.chunk.js"
+  },
+  {
+    "revision": "1ab70305e1b2e755ab07",
+    "url": "/dailynews/static/js/runtime~main.30c8aa4d.js"
+  }
+]);
